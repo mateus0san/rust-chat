ca1206c0d5339afe9b2ff8d498bc483c0255249a
Mateus S. Araujo <mateus0santiago@proton.me>
2026-02-18 10:47:18 -0300

    New attempt!

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,119 @@
 use std::{
-    io::{BufRead, BufReader, Read},
+    io::{self, BufRead, BufReader, BufWriter, Read, Write},
     net::{TcpListener, TcpStream},
-    sync::{Arc, Mutex, mpsc},
     thread,
 };
 
-use chat::{Client, Rooms, Text};
-
-const ADDRESS: &str = "0.0.0.0:1337";
-
 fn main() {
-    let (sender, receiver) = mpsc::channel();
-    let listener = TcpListener::bind(ADDRESS).expect("Could not bind address");
+    let listener = TcpListener::bind(server_address()).expect("ERROR: Failed to bind server");
+    let server_address = listener
+        .local_addr()
+        .expect("ERROR: Failed to find server address");
 
-    thread::spawn(move || {
-        for stream in listener.incoming() {
-            if let Ok(s) = stream
-                && sender.send(s).is_err()
-            {
-                eprintln!("INFO: receiver from listener disconnected.");
-                break;
+    println!("Serving on {server_address}");
+
+    for stream in listener.incoming() {
+        let stream = match stream {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("ERROR: {e}");
+                continue;
             }
-        }
-    });
-
-    let rooms = Arc::new(Mutex::new(Rooms::default()));
-
-    while let Ok(stream) = receiver.recv() {
-        let rooms = Arc::clone(&rooms);
-
+        };
         thread::spawn(move || {
-            let Ok(addr) = stream.peer_addr() else {
-                return;
-            };
-
-            let text_name = match get_client_name(&stream) {
-                Ok(name) => Text::try_new(name),
-                Err(e) => {
-                    eprintln!("ERROR: {addr} {e}",);
-                    return;
-                }
-            };
-
-            match text_name {
-                Ok(name) => start_chat(rooms, Client::new(name, stream)),
-                Err(_) => {
-                    let name = Text::try_new(String::from("guest")).expect("guest must be valid");
-                    start_chat(rooms, Client::new(name, stream))
-                }
-            }
+            handle_client(stream);
         });
     }
 }
 
-fn get_client_name(stream: &TcpStream) -> Result<String, std::io::Error> {
-    const USERNAME_LEN: usize = 25;
-
-    let mut name = String::with_capacity(USERNAME_LEN);
-    let mut buf_reader = BufReader::new(stream)
-        .take(u64::try_from(USERNAME_LEN).expect("USERNAME_LEN may convert to u64"));
-
-    buf_reader.read_line(&mut name)?;
-
-    Ok(name)
+fn server_address() -> String {
+    std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "0.0.0.0:1337".to_string())
 }
 
-fn start_chat(_rooms: Arc<Mutex<Rooms>>, _client: Client) {
-    todo!()
+struct Client {
+    username: String,
+    writer: TcpStream,
+    reader: BufReader<TcpStream>,
+    ip: String,
+}
+
+#[derive(Debug)]
+enum ClientError {
+    Io(io::Error),
+    Eof,
+}
+
+impl Client {
+    fn try_new(mut stream: TcpStream) -> Result<Self, ClientError> {
+        let ip = stream.peer_addr().map_err(ClientError::Io)?.to_string();
+
+        stream
+            .write_all("Type your username (max 16 characters): ".as_bytes())
+            .map_err(ClientError::Io)?;
+
+        const MAX_USERNAME_LEN: usize = 16;
+        let mut username = String::with_capacity(MAX_USERNAME_LEN);
+
+        let reader = stream.try_clone().map_err(ClientError::Io)?;
+        let mut buffer = BufReader::new(reader);
+        match buffer.read_line(&mut username) {
+            Ok(0) => Err(ClientError::Eof),
+            Err(e) => Err(ClientError::Io(e)),
+            Ok(_) => Ok(Client::new(username, stream, buffer, ip)),
+        }
+    }
+
+    fn new(username: String, writer: TcpStream, reader: BufReader<TcpStream>, ip: String) -> Self {
+        Self {
+            username,
+            writer,
+            reader,
+            ip,
+        }
+    }
+}
+
+fn handle_client(stream: TcpStream) {
+    let client = match Client::try_new(stream) {
+        Ok(client) => client,
+        Err(ClientError::Io(e)) => {
+            eprintln!("ERROR: client_handle std::io::Error {e}");
+            return;
+        }
+        Err(e) => {
+            eprint!("ERROR: client_handle");
+            dbg!(e);
+            return;
+        }
+    };
+
+    eprintln!(
+        "New client {} at the address {}",
+        client.username, client.ip
+    );
+
+    // const MAX_BYTES: usize = 120;
+    // let mut message = String::with_capacity(MAX_BYTES);
+
+    // loop {
+    //     match buffer.read_line(&mut message) {
+    //         Ok(0) => {
+    //             eprintln!("INFO: {} buffer returned EOF", client.ip);
+    //             break;
+    //         }
+    //         Ok(b) => eprintln!(
+    //             "INFO: Number of bytes read from {}: {b}:{}",
+    //             client.ip,
+    //             message.trim()
+    //         ),
+    //         Err(err) => {
+    //             eprintln!("ERROR: {err}");
+    //             break;
+    //         }
+    //     }
+
+    //     message.clear();
+    // }
 }
