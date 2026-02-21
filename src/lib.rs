5e32de353df4441ab116e5498a39e741ee51d339
Mateus S. Araujo <mateus0santiago@proton.me>
2026-02-21 13:21:47 -0300

    Refactoring and improving the drop of the clients

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,45 @@
+use std::io::{self, Write};
+use std::net::{SocketAddr, TcpStream};
 
+pub enum Message {
+    Broadcast(String),
+    NewClient(Client),
+    Drop(SocketAddr),
+}
+
+pub struct Client {
+    ip: SocketAddr,
+    writer: TcpStream,
+}
+
+impl Drop for Client {
+    fn drop(&mut self) {
+        eprintln!("INFO: dropping client with address {}", self.ip);
+        let _ = self.writer.shutdown(std::net::Shutdown::Both);
+    }
+}
+
+impl Client {
+    pub fn try_new(stream: TcpStream) -> Result<Client, io::Error> {
+        let ip = stream.peer_addr()?;
+
+        Ok(Client::new(stream, ip))
+    }
+
+    fn new(writer: TcpStream, ip: SocketAddr) -> Self {
+        Self { ip, writer }
+    }
+
+    pub fn write(&mut self, buf: &[u8]) -> std::io::Result<()> {
+        self.writer.write_all(buf)
+    }
+
+    pub fn ip(&self) -> SocketAddr {
+        self.ip
+    }
+}
+
+pub enum ConnectionEnd {
+    Normal,
+    ReceiverDropped,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
 use std::{
-    collections::HashMap,
-    io::{self, BufRead, BufReader, Read, Write},
+    collections::{HashMap, hash_map::Entry},
+    io::{self, BufRead, BufReader, Read},
     net::{SocketAddr, TcpListener, TcpStream},
-    sync::mpsc,
+    sync::mpsc::{self, Receiver},
     thread,
 };
+
+use chat::{Client, ConnectionEnd, Message};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:1337").expect("ERROR: could not start the server");
     let (sender, receiver) = mpsc::channel::<Message>();
 
-    thread::spawn(|| {
-        let mut clients = HashMap::new();
-        for msg in receiver {
-            match msg {
-                Message::Broadcast(msg) => new_message(msg, &mut clients),
-                Message::NewClient(client) => {
-                    if let Some(new_client) = new_client(&mut clients, client) {
-                        eprintln!("INFO: New client {}", new_client.ip);
-                    } else {
-                        eprintln!("INFO: Ip address of new client is already on the server.")
-                    }
-                }
-            }
-        }
-    });
+    thread::spawn(|| server(receiver));
 
     for stream in listener.incoming() {
         let sender = sender.clone();
@@ -33,81 +21,78 @@ fn main() {
             Ok(stream) => {
                 thread::spawn(|| match handle_connection(stream, sender) {
                     Err(e) => eprintln!("INFO: connection failed: {e}"),
-                    Ok(e) => eprintln!("INFO: {e}"),
+                    Ok(ConnectionEnd::ReceiverDropped) => {
+                        eprintln!("ERROR: Receiver Dropped, it should not happen")
+                    }
+                    _ => (),
                 });
             }
         }
     }
 }
 
-fn new_client(clients: &mut HashMap<SocketAddr, Client>, client: Client) -> Option<&mut Client> {
-    if clients.get(&client.ip).is_some() {
-        client
-            .writer
-            .shutdown(std::net::Shutdown::Both)
-            .expect("This stream must to shutdown");
-        return None;
+fn server(receiver: Receiver<Message>) {
+    let mut clients = HashMap::new();
+    for msg in receiver {
+        match msg {
+            Message::Broadcast(msg) => new_message(msg, &mut clients),
+            Message::Drop(ip) => {
+                clients.remove(&ip);
+            }
+            Message::NewClient(client) => {
+                if let Some(new_client) = new_client(&mut clients, client) {
+                    eprintln!("INFO: New client {}", new_client.ip());
+                } else {
+                    eprintln!("INFO: Ip address of new client is already on the server.")
+                }
+            }
+        }
     }
+}
 
-    Some(clients.entry(client.ip).or_insert(client))
+fn new_client(clients: &mut HashMap<SocketAddr, Client>, client: Client) -> Option<&mut Client> {
+    match clients.entry(client.ip()) {
+        Entry::Occupied(_) => None,
+        Entry::Vacant(e) => Some(e.insert(client)),
+    }
 }
 
 fn new_message(msg: String, clients: &mut HashMap<SocketAddr, Client>) {
     let msg = msg.as_bytes();
-    let _removed_clients: HashMap<SocketAddr, Client> = clients
-        .extract_if(|_k, v| v.writer.write_all(msg).is_err())
-        .collect();
-}
-
-enum Message {
-    Broadcast(String),
-    NewClient(Client),
-}
-
-struct Client {
-    ip: SocketAddr,
-    writer: TcpStream,
-}
-
-impl Client {
-    fn try_new(stream: TcpStream) -> Result<Client, io::Error> {
-        let ip = stream.peer_addr()?;
-
-        Ok(Client::new(stream, ip))
-    }
-
-    fn new(writer: TcpStream, ip: SocketAddr) -> Self {
-        Self { ip, writer }
-    }
+    let _removed_clients: HashMap<SocketAddr, Client> =
+        clients.extract_if(|_k, v| v.write(msg).is_err()).collect();
 }
 
 fn handle_connection(
     stream: TcpStream,
     sender: mpsc::Sender<Message>,
-) -> Result<&'static str, io::Error> {
-    let recv_msg = "receiver dropped";
-    let end_normal = "connection closed normally";
-
+) -> Result<ConnectionEnd, io::Error> {
     let reader = stream.try_clone()?;
 
     let client = Client::try_new(stream)?;
+    let ip = client.ip();
+
     if sender.send(Message::NewClient(client)).is_err() {
-        return Ok(recv_msg);
+        return Ok(ConnectionEnd::ReceiverDropped);
     }
 
     let mut reader = BufReader::new(reader);
 
-    loop {
+    let result = loop {
         let mut msg = String::with_capacity(120);
 
         match reader.by_ref().take(120).read_line(&mut msg) {
-            Ok(0) => break Ok(end_normal),
+            Ok(0) => break Ok(ConnectionEnd::Normal),
             Err(e) => break Err(e),
             _ => {
                 if sender.send(Message::Broadcast(msg)).is_err() {
-                    break Ok(recv_msg);
+                    break Ok(ConnectionEnd::ReceiverDropped);
                 }
             }
         }
-    }
+    };
+
+    let _ = sender.send(Message::Drop(ip));
+
+    result
 }
